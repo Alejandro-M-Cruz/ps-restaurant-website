0c26e7abf4af88b92c9d70f3d4be7f239913a5d9
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-03-14 11:25:18 +0000

    Cambios menu de secciones.

---
+++ javascript/menu-edit.js
@@ -0,0 +1,29 @@
+function loadContent(path) {
+    fetch(path).then(response => response.json()).then(data => {
+        loadPage(data)
+    })
+}
+
+function loadPage(pageContent) {
+    document.querySelector(".page-title").innerHTML = pageContent.title;
+    document.querySelector(".confirm-button").innerHTML = pageContent.addButtonLabel;
+    document.querySelector(".back-button").innerHTML = pageContent.editButtonLabel;
+    document.querySelector(".cancel-button").innerHTML = pageContent.deleteButtonLabel;
+    fetch("/demo-database/menu-sections.json").then(response => response.json()).then(data => {
+       let select = document.querySelector(".menu-form").querySelector("select");
+       let value = 0;
+       data.forEach(element => {
+            let section = document.createElement("option");
+            section.nodeValue = value++;
+            section.innerHTML = element.sectionName;
+            select.appendChild(section);
+       });
+       
+    })
+    document.querySelector(".confirm-button").addEventListener("click",() => redirectPage(true))
+}
+
+
+function redirectPage(isNew){
+    window.location.href = "menu-items-edit.html?isNew=" + isNew;
+}
