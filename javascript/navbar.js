aa46d9ef5efc4abb98a4b461f11055d1aac8c696
Cristhian López <holbey12@gmail.com>
2023-03-20 17:59:55 +0100

    comment navbar js and small change in css

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,3 +1,8 @@
+// Almacena los elementos de la lista del navegador excepto el LOGO
+// Cuando apretamos el boton que lanza el DESPLEGABLE de opciones,
+// Cambiamos entre que se vean en la pantalla o no
+// Por defecto antes de descargar la página, esos elementos no se podran ver
+
 function initNavbar() {
     const navbarItems = document.querySelectorAll(".navbar-list li:not(.navbar-list__logo)")
     document.querySelector(".hamburger-button").onclick = () => {
